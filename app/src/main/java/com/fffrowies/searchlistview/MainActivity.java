fd579a73daad665bec1ea26c4f3b8ffd35f17dca
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-11 15:20:53 -0300

    Add model and adapter class

--- app/src/main/java/com/fffrowies/searchlistview/MainActivity.java
+++ app/src/main/java/com/fffrowies/searchlistview/MainActivity.java
@@ -4,14 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    ListViewAdapter adapter;
+    String[] title;
+    String[] description;
+    int[] icon;
+    ArrayList<Model> arrayList = new ArrayList<Model>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = new String[]{};
+        description = new String[]{};
+        icon = new int[]{};
 
         listView = findViewById(R.id.listView);
     }
